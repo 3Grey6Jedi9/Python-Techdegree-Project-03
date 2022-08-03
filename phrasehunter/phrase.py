7c627343f8b74346a0eff9f08fd8f6d9028e8883
Darth Vader <danieltarancon@gmail.com>
2022-08-03 12:54:49 +0200

    Update phrase.py

--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,40 @@
-# Create your Phrase class logic here.
+class Phrase():
+
+
+    def __init__(self, phrase):
+
+        self.phrase = phrase.lower()
+
+
+    def display(self, guesses):
+
+        for letter in self.phrase:
+            if letter in guesses:
+                print(f'{letter}', end = "")
+            elif letter == ' ':
+                print(" ", end ="")
+            else:
+                print("_", end = " ")
+
+        print('\n')
+
+
+    def check_guess(self, guess):
+
+        if guess in self.phrase:
+            return True
+        else:
+            return False
+
+    def check_complete(self, guesses):
+
+        for letter in self.phrase:
+            if letter not in guesses:
+                return False
+            else:
+                guesses.append(' ')
+                if set(guesses) >= set(self.phrase):
+                    return True
+                else:
+                    return False
+                
